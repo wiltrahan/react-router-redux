b29e737204f69e4f5929bca08d7d007587ce13b7
wiltrahan <twilorip@gmail.com>
2018-06-26 15:41:30 -0400

    input filters

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import confgureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -10,15 +11,24 @@ import './styles/styles.scss';
 
 const store = confgureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill '}));
-
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500}));
 store.dispatch(addExpense({ description: 'Gas Bill '}));
 store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('bill'));
+}, 3000)
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 //console.log(store.getState());
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
 
