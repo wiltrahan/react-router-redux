0d8410c9bb184f2d0153d3e3003dc5b58dedc25b
wiltrahan <twilorip@gmail.com>
2018-06-26 12:19:46 -0400

    sort redux data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -141,6 +141,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
     return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if(sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1;
+    }
   });
 };
 
